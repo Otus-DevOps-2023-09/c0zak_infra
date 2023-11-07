4b49dabad0bb9d126b3f1ee481a7e04b3d6ef4d3
Vasiliy Motchenko <vasiliy.motchenko@gmail.com>
2023-11-07 14:47:42 +0300

    Homework 7.fix for sloppy test

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "cloud_id" {
+  description = "Cloud"
+}
+variable "folder_id" {
+  description = "Folder"
+}
+variable "zone" {
+  description = "Zone"
+}
+variable "keyfile_path" {
+  # Описание переменной
+  description = "Path to the API key file"
+}
+variable "service_account_id" {
+  description = "Service account ID"
+}
+variable "reddit-bucket" {
+  description = "Bucket for save states"
+}
