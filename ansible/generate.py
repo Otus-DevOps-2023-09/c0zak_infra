ea59f7cc29c190d810a4c486f5a46409d57858d2
Vasiliy Motchenko <vasiliy.motchenko@gmail.com>
2023-11-16 23:31:39 +0300

    Homework 8.fix

--- ansible/generate.py
+++ ansible/generate.py
@@ -6,36 +6,35 @@ import subprocess
 import sys
 
 def main():
-   if len(sys.argv) > 1 and sys.argv[1] == "--list":
-       data = {
-           "_meta": {
-               "hostvars": {}
-           },
-           "all": {
-               "children": ["app", "db"]
-           },
-           "app": {
-               "hosts": []
-           },
-           "db": {
-               "hosts": []
-           }
-       }
+  if len(sys.argv) > 1 and sys.argv[1] == "--list":
+      data = {
+          "_meta": {
+              "hostvars": {}
+          },
+          "all": {
+              "children": ["app", "db"]
+          },
+          "app": {
+              "hosts": []
+          },
+          "db": {
+              "hosts": []
+          }
+      }
 
+      os.chdir("../terraform/stage")
 
-   os.chdir("../terraform/stage")
-   output = subprocess.check_output(["terraform", "output", "-json"]).decode()
-   output_dict = json.loads(output)
-   try:
-        data["app"]["hosts"].append(output_dict["external_ip_address_app"]["value"])
-        data["db"]["hosts"].append(output_dict["external_ip_address_db"]["value"])
+      if os.path.isfile(".terraform.lock.hcl"):
+          output = subprocess.check_output(["terraform", "output", "-json"]).decode()
+          output_dict = json.loads(output)
 
-   except Exception as e:
-        data["app"]["hosts"].append("1.2.3.4")
-        data["db"]["hosts"].append("4.3.2.1")
+          data["app"]["hosts"].append(output_dict["external_ip_address_app"]["value"])
+          data["db"]["hosts"].append(output_dict["external_ip_address_db"]["value"])
+      else:
+          data["app"]["hosts"].append("1.2.3.4")
+          data["db"]["hosts"].append("4.3.2.1")
 
-
-   print(json.dumps(data, indent=4))
+      print(json.dumps(data, indent=4))
 
 if __name__ == "__main__":
-   main()
+  main()
