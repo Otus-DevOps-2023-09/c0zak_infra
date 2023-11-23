5a0446fde79c3c7e711b957f5469e643fafb284c
Vasiliy Motchenko <vasiliy.motchenko@gmail.com>
2023-11-23 22:17:17 +0300

    Homework 9

--- terraform/modules/db/main.tf
+++ terraform/modules/db/main.tf
@@ -10,7 +10,7 @@
 resource "yandex_compute_instance" "db" {
   name = "reddit-db"
   labels = {
-    tags = "reddit-db"
+    tags = "db"
   }
   metadata = {
     ssh-keys = "ubuntu:${file(var.public_key_path)}"
@@ -26,12 +26,12 @@ resource "yandex_compute_instance" "db" {
     agent       = false
     private_key = file(var.private_key_path)
   }
-  provisioner "remote-exec" {
-    inline = [
-      "sudo sed -i 's/127.0.0.1/${yandex_compute_instance.db.network_interface.0.ip_address}/g' /etc/mongodb.conf",
-      "sudo systemctl restart mongodb"
-    ]
-  }
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "sudo sed -i 's/127.0.0.1/${yandex_compute_instance.db.network_interface.0.ip_address}/g' /etc/mongodb.conf",
+  #     "sudo systemctl restart mongodb"
+  #   ]
+  # }
   boot_disk {
     initialize_params {
       image_id = var.db_disk_image
